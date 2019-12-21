5c9a0d18c82f03047ee18ce5f1fee3925a9b0c0d
bhaskar5707 <sunilhrit143@gmail.com>
2019-12-21 23:12:40 +0530

    Initial commit

---
+++ src/components/Cart.js
@@ -0,0 +1,12 @@
+import React , { Component } from 'react';
+export default class Cart extends React.Component {
+
+
+	render() {
+		return (
+			<div>
+                <h3>cart</h3>
+			</div>
+		);
+	}
+}
---
+++ src/components/Modal.js
@@ -0,0 +1,14 @@
+import React , { Component } from 'react';
+
+export default class Modal extends React.Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div></div>
+		);
+	}
+}
